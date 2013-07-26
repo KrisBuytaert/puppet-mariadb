91c74c996150abb79fb6031a54a44cbfd1b5b153
Stephen Brown II <Stephen.Brown2@gmail.com>
2013-07-25 23:22:39 -0400

    Puppet Lint cleanup

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 class maria::yumrepository {
 
-  $release = "5.5"
+  $release = '5.5'
 
-  $os   = $::operatingsystem ? {
+  $os = $::operatingsystem ? {
     'RedHat'    => 'rhel',
     'LinuxMint' => 'ubuntu',
     default     => inline_template('<%= @operatingsystem.downcase %>'),
@@ -11,31 +11,24 @@ class maria::yumrepository {
   $arch = $::architecture ? {
     /^.*86$/ => 'x86',
     /^.*64$/ => 'amd64',
-    default   => $::architecture,
+    default  => $::architecture,
   }
 
-   yumrepo {
-
-
-	 
-   "mariadb":
-            descr       => "MariaDB Yum Repo",
-            enabled     => 1,
-            gpgcheck    => 1,
-            gpgkey      => 'https://yum.mariadb.org/RPM-GPG-KEY-MariaDB',
-            baseurl     => "http://yum.mariadb.org/${release}/${os}${::lsbmajdistrelease}-${arch}";
-        
-
-}
-
+  yumrepo { 'mariadb':
+    descr    => 'MariaDB Yum Repo',
+    enabled  => 1,
+    gpgcheck => 1,
+    gpgkey   => 'https://yum.mariadb.org/RPM-GPG-KEY-MariaDB',
+    baseurl  => "http://yum.mariadb.org/${release}/${os}${::lsbmajdistrelease}-${arch}",
+  }
 
 }
 
 class maria::aptrepo {
 
-  $release = "5.5"
+  $release = '5.5'
 
-  $os   = $::operatingsystem ? {
+  $os = $::operatingsystem ? {
     'RedHat'    => 'rhel',
     'LinuxMint' => 'ubuntu',
     default     => inline_template('<%= @operatingsystem.downcase %>'),
@@ -44,50 +37,44 @@ class maria::aptrepo {
   $arch = $::architecture ? {
     /^.*86$/ => 'x86',
     /^.*64$/ => 'amd64',
-    default   => $::architecture,
+    default  => $::architecture,
   }
 
-	exec {"Import MariaDB Apt repo key":
-		command => "/usr/bin/apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db && /usr/bin/touch /root/.my.keyimported",
-		refreshonly => true,
-		creates => "/root/.my.keyimported",
-	}
+  exec {'Import MariaDB Apt repo key':
+    command     => '/usr/bin/apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db && /usr/bin/touch /root/.my.keyimported',
+    refreshonly => true,
+    creates     => '/root/.my.keyimported',
+  }
 
-
-	file { "/etc/apt/sources.list.d/maria.list":	
-		content => "# MariaDB repository list
+  file { '/etc/apt/sources.list.d/maria.list':
+    content => "# MariaDB repository list
 # http://mariadb.org/mariadb/repositories/
 deb http://ftp.osuosl.org/pub/mariadb/repo/${release}/${os} ${::lsbdistcodename} main
-deb-src http://ftp.osuosl.org/pub/mariadb/repo/${release}/${os} ${::lsbdistcodename} main";
-		
-	
-	}
-	
-
+deb-src http://ftp.osuosl.org/pub/mariadb/repo/${release}/${os} ${::lsbdistcodename} main",
+  }
 
 }
+
 class maria::debpackages {
-	package { "mariadb-server":
-			ensure => "installed";
-		  "mariadb-client":
-			ensure => "installed";
-		
-	}
+
+  package { 'mariadb-server':
+    ensure => 'installed',
+  }
+  package { 'mariadb-client':
+    ensure => 'installed',
+  }
 
 }
 
 class maria::rpmpackages {
 
+  package { 'MariaDB-server':
+    ensure => 'installed',
+    alias  => 'MySQL-server',
+  }
+  package { 'MariaDB-client':
+    ensure => 'installed',
+    alias  => 'MySQL-client',
+  }
 
-   $basearch = "i386"
-	package { "MariaDB-server":
-            		alias  => "MySQL-server",
-			ensure => "installed";
-		"MariaDB-client":
-            		alias  => "MySQL-client",
-			ensure => "installed";
-
-	}
-	
 }
-	
